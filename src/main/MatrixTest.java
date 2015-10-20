45756561a2cf78fe9d0049400d4e3281eaca6dd8
Ivan Antamanov <ivan.antamanov@gmail.com>
2015-10-20 14:43:04 +0300

    Matrix

---
+++ src/main/MatrixTest.java
@@ -0,0 +1,29 @@
+package main;
+
+import main.MatrixExceptions.WrongMultiplicationMatrixException;
+import main.MatrixExceptions.WrongSumMatrixException;
+
+/**
+ * Created by iantaman on 19.10.2015.
+ */
+public class MatrixTest {
+    private int[][] matrix1;
+    private int[][] matrix2;
+    private int[][] matrix3; //matrix3 = matrix1 * matrix2
+    private int[][] matrix4; //matrix4 = matrix1 + matrix2
+
+
+    public static void main(String[] args) {
+        MatrixTest matrixTest = new MatrixTest();
+
+        matrixTest.matrix1 = MatrixUtility.createMatrix(5, 5);
+        matrixTest.matrix2 = MatrixUtility.createMatrix(5, 5);
+        try {
+            matrixTest.matrix3 = MatrixUtility.multiplicationMatrix(matrixTest.matrix1, matrixTest.matrix2);
+            matrixTest.matrix4 = MatrixUtility.sumOfMatrix(matrixTest.matrix1, matrixTest.matrix2);
+            MatrixUtility.transposeMatrix(matrixTest.matrix3);
+        } catch (WrongMultiplicationMatrixException | WrongSumMatrixException ex1) {
+            ex1.printStackTrace();
+        }
+    }
+}
